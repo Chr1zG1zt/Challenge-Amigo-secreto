6d382f093339338639173d17a6b00d8780956c56
Chr1zG1zt <197325657+Chr1zG1zt@users.noreply.github.com>
2025-02-01 18:00:48 +0000

    crea función para sortear amigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -41,17 +41,7 @@ function limpiarInput(id) {
     document.getElementById(id).value = "";
 }
 
-/*Escribe una función que seleccione de manera aleatoria uno de los nombres almacenados en el array amigos. Usa Math.random() y Math.floor() para obtener un índice aleatorio.
-Tareas específicas:
 
-Validar que haya amigos disponibles: Antes de sortear, comprobar si el array amigos no está vacío.
-
-Generar un índice aleatorio: Usar Math.random() y Math.floor() para seleccionar un índice aleatorio del arreglo.
-
-Obtener el nombre sorteado: Utilizar el índice aleatorio para acceder al nombre correspondiente en el arreglo.
-
-Mostrar el resultado: Actualizar el contenido del elemento de resultado utilizando document.getElementById()  e innerHTML para mostrar el amigo sorteado.
-*/
 function sortearAmigo() {
     if (amigos.length === 0) {
         alert("No hay amigos disponibles.");
